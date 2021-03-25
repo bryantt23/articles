dab178e0a9f64dffb5cffda7f5d5ccb42e8577ec
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-24 19:19:02 -0700

    clean up

--- frontend/src/components/Article/AddArticle.js
+++ frontend/src/components/Article/AddArticle.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { Category, Status } from '../../models/Enums';
 import { notificationArticle } from '../../reducers/notifications_reducer';
 import { connect } from 'react-redux';
