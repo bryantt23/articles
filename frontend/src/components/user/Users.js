09133ab544697e5792418733e627b649f0ab4d83
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-17 18:01:35 -0700

    show user articles as json

--- frontend/src/components/user/Users.js
+++ frontend/src/components/user/Users.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getUsers } from '../../util/users_api_util';
+import { getArticlesByUser } from '../../util/article_api_util';
 import User from '../user/User';
+import ArticlePreviewContainer from '../article/ArticlePreviewContainer';
 
 function Users() {
   const [users, setUsers] = useState(null);
@@ -9,6 +11,13 @@ function Users() {
     async function fetchData() {
       try {
         const usersFromApi = await getUsers();
+        for (const user of usersFromApi) {
+          console.log(user);
+          const articles = await getArticlesByUser(user._id);
+          user.articles = articles;
+          console.log(articles);
+        }
+        console.log(usersFromApi);
         setUsers(usersFromApi);
       } catch (error) {
         console.log(error);
@@ -26,7 +35,15 @@ function Users() {
           <h1>Users</h1>
           {users.map(user => {
             return (
-              <User key={user._id} handle={user.handle} email={user.email} />
+              <div key={user._id}>
+                {/* {JSON.stringify(user.articles)} */}
+                <User handle={user.handle} email={user.email} />
+                <div>
+                  {user.articles.length === 0
+                    ? 'There are no articles by this user'
+                    : JSON.stringify(user.articles)}
+                </div>{' '}
+              </div>
             );
           })}
         </div>
