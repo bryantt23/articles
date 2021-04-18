84a3435f146f13240722feabe67221b67a46ccbf
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-17 18:21:28 -0700

    Add mini preview

--- frontend/src/components/user/Users.js
+++ frontend/src/components/user/Users.js
@@ -3,6 +3,8 @@ import { getUsers } from '../../util/users_api_util';
 import { getArticlesByUser } from '../../util/article_api_util';
 import User from '../user/User';
 import ArticlePreviewContainer from '../article/ArticlePreviewContainer';
+import ArticlePreview from '../article/ArticlePreview';
+import { NavLink } from 'react-router-dom';
 
 function Users() {
   const [users, setUsers] = useState(null);
@@ -39,9 +41,30 @@ function Users() {
                 {/* {JSON.stringify(user.articles)} */}
                 <User handle={user.handle} email={user.email} />
                 <div>
-                  {user.articles.length === 0
-                    ? 'There are no articles by this user'
-                    : JSON.stringify(user.articles)}
+                  {user.articles.length === 0 ? (
+                    'There are no articles by this user'
+                  ) : (
+                    <div style={{ marginLeft: '20px' }}>
+                      User has the following articles:
+                      {user.articles.map(article => {
+                        return (
+                          <div
+                            key={article._id}
+                            style={{ border: '1px solid' }}
+                          >
+                            <h3>Title: {article.title}</h3>
+                            <p>Description: {article.description}</p>
+                            <NavLink
+                              to={`/articles/${article._id}`}
+                              activeClassName='active'
+                            >
+                              Go to article
+                            </NavLink>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  )}
                 </div>{' '}
               </div>
             );
