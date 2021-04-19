a3af7a665b8528db703ad663c0d58ee3ae892764
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-18 22:42:55 -0700

    Use https://fkhadra.github.io/react-toastify/introduction/

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -15,12 +15,15 @@ import Notification from './notification/Notification';
 import UsersContainer from './user/UsersContainer';
 
 import { Switch } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
       <NavBarContainer />
       <Notification />
+      <ToastContainer autoClose={10000} />
 
       <Switch>
         <AuthRoute exact path='/' component={MainPage} />
