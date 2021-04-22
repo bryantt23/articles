1ae2adea8ef19302d3c5eb8cc412f5c1e2dd7bbd
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-22 15:57:54 -0700

    from class to hooks

--- frontend/src/components/shared/withLoadingSpinner.js
+++ frontend/src/components/shared/withLoadingSpinner.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './loading.css';
 
 function withLoadingSpinner(WrappedComponent) {
-  return class extends React.Component {
-    render() {
-      if (!this.props.data) {
-        return <div className='loader'></div>;
-      } else {
-        return <WrappedComponent {...this.props} />;
-      }
+  return function (props) {
+    if (!props.data) {
+      return <div className='loader'></div>;
+    } else {
+      return <WrappedComponent {...props} />;
     }
   };
 }
