9d695d34abc2327297f0876ac510999b1d89d377
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-22 14:21:04 -0700

    convert to functional component

--- frontend/src/components/user/Authorization.js
+++ frontend/src/components/user/Authorization.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Authorization = allowedRoles => WrappedComponent => {
-  class WithAuthorization extends React.Component {
-    render() {
-      const { user } = this.props;
-
-      if (user.roles && user.roles.includes(allowedRoles)) {
-        return <WrappedComponent />;
-      }
-      return <div>This is not part of your role</div>;
+  function WithAuthorization({ user }) {
+    if (user.roles && user.roles.includes(allowedRoles)) {
+      return <WrappedComponent />;
     }
+    return <div>This is not part of your role</div>;
   }
 
   const mapStateToProps = state => {
