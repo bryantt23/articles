13f8495db99316b9aa16213d4d91b45b1bad0fd1
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-22 18:45:02 -0700

    custom hook for fetch and loading using https://www.smashingmagazine.com/2020/07/custom-react-hook-fetch-cache-data/

--- frontend/src/components/Article/Article.js
+++ frontend/src/components/Article/Article.js
@@ -6,13 +6,26 @@ import { getArticle } from '../../util/article_api_util';
 import Comments from '../comment/Comments';
 import labels from '../../constants/Labels';
 import { isArticleAuthor } from '../../util/article_util';
+import useFetch from '../shared/useFetch';
 
 function Article({ userId }) {
-  const [article, setArticle] = useState(null);
-  const [selectedLabels, setSelectedLabels] = useState(new Set());
   let { id } = useParams();
+  // const res = useFetch(() => getArticle(id).response);
+  const { response, error, loader } = useFetch(() => getArticle(id));
+  console.log('response', response);
+  console.log('loader', loader);
+  // console.log(typeof response);
+  const [article, setArticle] = useState(response);
+  console.log(article);
+  useEffect(() => {
+    console.log(typeof response);
+    setArticle(response);
+  }, [response]);
+  const [selectedLabels, setSelectedLabels] = useState(new Set());
   console.log('userId', userId);
 
+  console.log(article);
+  /*
   useEffect(() => {
     console.log(id);
     async function fetchData() {
@@ -30,9 +43,58 @@ function Article({ userId }) {
     }
     fetchData();
   }, []);
-
+*/
   return (
     <div className='App'>
+      {JSON.stringify(article)}
+      {article.length === 0 ? (
+        loader
+      ) : !article.data ? (
+        `There is no article with id ${id}`
+      ) : (
+        <div>
+          <h1>Article</h1>
+          <p>Title: {article.data.title}</p>
+          <p>Description: {article.data.description}</p>
+          <p>Status: {article.data.status}</p>
+          <p>Category: {article.data.category}</p>
+          <p>Labels: {JSON.stringify(article.data.labels)}</p>
+          <div>
+            <p>
+              Selected Labels:
+              {labels.map((label, i) => (
+                <span key={i} style={{ opacity: 0.5 }}>
+                  <input
+                    type='checkbox'
+                    checked={selectedLabels.has(label)}
+                    readOnly={true}
+                  ></input>
+                  {label}
+                </span>
+              ))}
+            </p>
+          </div>
+          <p>Is Deleted?: {article.data.isDeleted ? 'Yes' : 'No'}</p>
+          {isArticleAuthor(userId, article.data.author) && (
+            <NavLink
+              to={`/edit-article/${article.data._id}`}
+              activeClassName='active'
+            >
+              Edit article
+            </NavLink>
+          )}
+
+          <Comments
+            comments={article.data.comments}
+            articleId={article.data._id}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+/*
+
       {!article ? (
         `There is no article with id ${id}`
       ) : (
@@ -71,10 +133,9 @@ function Article({ userId }) {
           <Comments comments={article.comments} articleId={article._id} />
         </div>
       )}
-    </div>
-  );
-}
+    
 
+*/
 const mapStateToProps = state => {
   console.log(state);
   return {
