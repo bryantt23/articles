1981f2c68bd177c9a6b63aa76c9b6dc895a24edc
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-24 23:32:55 -0700

    Get email validation to work via Formik

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Formik } from 'formik';
+
 class SignupForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
       handle: '',
       password: '',
       password2: '',
@@ -25,14 +26,14 @@ class SignupForm extends Component {
     return e => this.setState({ [type]: e.currentTarget.value });
   }
 
-  handleSumbit(e) {
-    e.preventDefault();
+  handleSumbit({ email }) {
     let user = {
-      email: this.state.email,
+      email,
       handle: this.state.handle,
       password: this.state.password,
       password2: this.state.password2
     };
+    console.log(user);
     this.props.signup(user, this.props.history);
   }
 
@@ -49,41 +50,77 @@ class SignupForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSumbit}>
-          <div>
-            <br />
-            <input
-              type='text'
-              value={this.state.email}
-              onChange={this.update('email')}
-              placeholder='Email'
-            />
-            <br />
-            <input
-              type='text'
-              value={this.state.handle}
-              onChange={this.update('handle')}
-              placeholder='Handle'
-            />
-            <br />
-            <input
-              type='password'
-              value={this.state.password}
-              onChange={this.update('password')}
-              placeholder='Password'
-            />
-            <br />
-            <input
-              type='password'
-              value={this.state.password2}
-              onChange={this.update('password2')}
-              placeholder='Confirm Password'
-            />
-            <br />
-            <input type='submit' value='Submit' />
-            {this.renderErrors()}
-          </div>
-        </form>
+        <Formik
+          initialValues={{ email: '' }}
+          validate={values => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              this.handleSumbit(values);
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <br />
+                <input
+                  type='email'
+                  name='email'
+                  value={values.email}
+                  onChange={handleChange}
+                  placeholder='Email'
+                />
+                <br />
+                <input
+                  type='text'
+                  value={this.state.handle}
+                  onChange={this.update('handle')}
+                  placeholder='Handle'
+                />
+                <br />
+                <input
+                  type='password'
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  placeholder='Password'
+                />
+                <br />
+                <input
+                  type='password'
+                  value={this.state.password2}
+                  onChange={this.update('password2')}
+                  placeholder='Confirm Password'
+                />
+                <br />
+                {this.renderErrors()}
+                {errors.password && touched.password && errors.password}
+                <button type='submit' disabled={isSubmitting}>
+                  Submit
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     );
   }
